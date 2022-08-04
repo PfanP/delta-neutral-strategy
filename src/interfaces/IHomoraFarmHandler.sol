1755fcf47368019ae8f2fd5a13e82173a4c257f3
PfanP <36354694+PfanP@users.noreply.github.com>
2022-08-05 02:55:27 +1000

    fix: HomoraFarmHandler interface

--- src/interfaces/IHomoraFarmHandler.sol
+++ src/interfaces/IHomoraFarmHandler.sol
@@ -88,5 +88,5 @@ interface IHomoraFarmHandler {
 
     /// @dev Return the value of the given input as ETH per unit, multiplied by 2**112.
     /// @param token The ERC-20 token to check the value.
-    function getETHPx(address token) external view returns (uint)
+    function getETHPx(address token) external view returns (uint);
 }
